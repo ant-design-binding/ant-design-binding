6e25ffe56927a1ffa744e7410a4cb085e49b06ca
lxohi <16010388+lxohi@users.noreply.github.com>
2019-01-15 08:34:54 +0800

    Menu: adds .navigation

--- adb-component/src/main/scala/adb/component/menu/Menu.scala
+++ adb-component/src/main/scala/adb/component/menu/Menu.scala
@@ -1,9 +1,8 @@
 package adb.component.menu
 
 import com.thoughtworks.binding.{dom, Binding}
-import com.thoughtworks.binding.Binding.BindingSeq
-import org.scalajs.dom.raw.Node
-import org.scalajs.dom.Event
+import com.thoughtworks.binding.Binding.{BindingSeq, Constant, Constants, SingletonBindingSeq, Var}
+import org.scalajs.dom.raw.{Event, Node}
 
 object Menu {
 
@@ -15,10 +14,31 @@ object Menu {
   }
 
   @dom
-  def menuItem(style: String = "", onclick: Event => Unit = _ => {}, children: BindingSeq[Node]): Binding[Node] = {
-    <li class="ant-menu-item" style={style} onclick={onclick}>
+  def menuItem(`class`: Binding[String] = Constant(""), style: Binding[String] = Constant(""), onclick: Event => Unit = _ => {})(
+      children: BindingSeq[Node]): Binding[Node] = {
+    <li class={"ant-menu-item " + `class`.bind} style={style.bind} onclick={onclick}>
       {children.map(v => v)}
     </li>
+  }
+
+  case class NavigationItem[T](selectedItem: T, content: Binding[Node])
+
+  def navigation[T](navigationItems: List[NavigationItem[T]], selectedItem: Var[Option[T]]): Binding[Node] = {
+    menu {
+      for {
+        ni <- Constants(navigationItems: _*)
+      } yield {
+        menuItem(
+          `class` = Constant(if (selectedItem.bind.contains(ni.selectedItem)) "ant-menu-item-selected" else ""),
+          style = Constant("padding-left: 24px;"),
+          onclick = (_: Event) => {
+            selectedItem.value = Some(ni.selectedItem)
+          }
+        ) {
+          SingletonBindingSeq(ni.content)
+        }.bind
+      }
+    }
   }
 
 }
