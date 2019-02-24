182aea5cb647ea4663ff68023b49d3d4f151a053
lxohi <16010388+lxohi@users.noreply.github.com>
2019-02-24 18:23:12 +0800

    Router: fix overflow in Path.subPath

--- adb-component/src/main/scala/adb/router/Router.scala
+++ adb-component/src/main/scala/adb/router/Router.scala
@@ -39,7 +39,7 @@ trait Directive[T] {
 
 case class Path(segments: Seq[String]) {
   def subPath(start: Int, len: Int = Int.MaxValue): Path = {
-    Path(segments.slice(start, start + len))
+    Path(segments.drop(start).take(len))
   }
 }
 
